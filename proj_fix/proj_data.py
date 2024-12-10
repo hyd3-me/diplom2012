21af249666a5b661cd226154426e2acf0880ef48
sedg <hyde.rou@yandex.ru>
2024-12-11 00:03:59 +0300

    add data files for project with CONSTANT names: LOGIN_PATH, LOGIN_HTML etc...

---
+++ proj_fix/proj_data.py
@@ -0,0 +1,9 @@
+## this file contains data for the entire project.
+## such as paths, messages to the user
+
+LOGIN_PATH              = 'login'
+HOME_PATH               = 'index'
+PROFILE_PATH            = 'profile'
+
+
+SUCCESS_LOG             = 'welcome to join us'
