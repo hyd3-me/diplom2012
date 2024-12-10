4c1d1d82add928fcf793070b81d34f68bc2a774b
sedg <hyde.rou@yandex.ru>
2024-12-11 00:05:11 +0300

    create test dir for app_profile: done first test for check has login page

--- app_profile/tests/test_base_user.py
+++ app_profile/tests/test_base_user.py
@@ -2,9 +2,15 @@ from django.test import TestCase
 from django.urls import reverse
 from unittest import skip
 
+from proj_fix import proj_data as data, template_name as template
 
-class UserTest(TestCase):
+
+class BaseUser(TestCase):
+    pass
+
+class UserTest(BaseUser):
 
     def test_has_login_page(self):
-        response = self.client.get(reverse(proj_data.LOGIN_PATH))
+        response = self.client.get(reverse(data.LOGIN_PATH))
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, template.LOGIN_HTML)
