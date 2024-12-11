3c69475994d2beed18cddfaf8a2518273accd8ac
sedg <hyde.rou@yandex.ru>
2024-12-11 22:45:03 +0300

    done grouptest: chekc wrong pwd for group return err=True

--- app_fixgroups/tests/tests_inner.py
+++ app_fixgroups/tests/tests_inner.py
@@ -10,3 +10,8 @@ class UserTest(TestCase):
         err, group = utils.create_group(data.GROUP1[0], data.GROUP1[1])
         err, group = utils.check_pwd4group(group, data.GROUP1[1])
         self.assertFalse(err)
+    
+    def test_check_wrong_pwd_for_group(self):
+        err, group = utils.create_group(data.GROUP1[0], data.GROUP1[1])
+        err, group = utils.check_pwd4group(group, 'anyWrongPWD')
+        self.assertTrue(err)
