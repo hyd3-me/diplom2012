d3f8fba174588b753f6ef38b495db76b40a1754c
sedg <hyde.rou@yandex.ru>
2024-12-11 23:08:08 +0300

    done grouptest: can_get_staff_from_user

--- app_fixgroups/tests/tests_inner.py
+++ app_fixgroups/tests/tests_inner.py
@@ -23,3 +23,11 @@ class StaffTest(TestCase):
         err, group = utils.create_group(data.GROUP1[0], data.GROUP1[1])
         err, staff = utils.create_staff(group, user)
         self.assertFalse(err)
+    
+    def test_can_get_staff_from_user(self):
+        err, user = utils.create_user(data.USER1)
+        err, group = utils.create_group(data.GROUP1[0], data.GROUP1[1])
+        err, staff = utils.create_staff(group, user)
+        err, q_staff_from_user = utils.get_staff_by_user(user)
+        self.assertFalse(err)
+        self.assertEqual(staff, q_staff_from_user[0])
