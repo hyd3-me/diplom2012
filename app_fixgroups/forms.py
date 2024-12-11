a40e0231b9f1522d89b0854153415687af78103c
sedg <hyde.rou@yandex.ru>
2024-12-12 00:55:14 +0300

    added GroupForm for join_group_view

--- app_fixgroups/forms.py
+++ app_fixgroups/forms.py
@@ -6,9 +6,15 @@ from .models import FixGroup
 
 class GroupCreationForm(forms.ModelForm):
 
-    group_pwd = forms.CharField(label='Пароль', max_length=64,
+    group_pwd   = forms.CharField(label='password', max_length=64,
             widget=forms.PasswordInput())
 
     class Meta:
-        model = FixGroup
-        fields = ('name',)
+        model   = FixGroup
+        fields  = ('name',)
+
+class GroupForm(forms.Form):
+
+    name        = forms.CharField(label='name', max_length=32)
+    group_pwd   = forms.CharField(label='password', max_length=64,
+            widget=forms.PasswordInput())
