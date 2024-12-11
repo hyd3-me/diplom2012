abc2b4a7831caf717e17ecb0aab3ac298a53d020
sedg <hyde.rou@yandex.ru>
2024-12-11 23:33:17 +0300

    done grouptest: group has owner

--- app_fixgroups/tests/tests_inner.py
+++ app_fixgroups/tests/tests_inner.py
@@ -7,12 +7,14 @@ from app_profile import utils
 class GroupTest(TestCase):
 
     def test_check_pwd_for_group(self):
-        err, group = utils.create_group(data.GROUP1[0], data.GROUP1[1])
+        err, user = utils.create_user(data.USER1)
+        err, group = utils.create_group(data.GROUP1[0], data.GROUP1[1], user)
         err, group = utils.check_pwd4group(group, data.GROUP1[1])
         self.assertFalse(err)
     
     def test_check_wrong_pwd_for_group(self):
-        err, group = utils.create_group(data.GROUP1[0], data.GROUP1[1])
+        err, user = utils.create_user(data.USER1)
+        err, group = utils.create_group(data.GROUP1[0], data.GROUP1[1], user)
         err, group = utils.check_pwd4group(group, 'anyWrongPWD')
         self.assertTrue(err)
 
@@ -20,14 +22,19 @@ class StaffTest(TestCase):
 
     def test_can_create_staff(self):
         err, user = utils.create_user(data.USER1)
-        err, group = utils.create_group(data.GROUP1[0], data.GROUP1[1])
+        err, group = utils.create_group(data.GROUP1[0], data.GROUP1[1], user)
         err, staff = utils.create_staff(group, user)
         self.assertFalse(err)
     
     def test_can_get_staff_from_user(self):
         err, user = utils.create_user(data.USER1)
-        err, group = utils.create_group(data.GROUP1[0], data.GROUP1[1])
+        err, group = utils.create_group(data.GROUP1[0], data.GROUP1[1], user)
         err, staff = utils.create_staff(group, user)
         err, q_staff_from_user = utils.get_staff_by_user(user)
         self.assertFalse(err)
         self.assertEqual(staff, q_staff_from_user[0])
+    
+    def test_has_owner_for_group(self):
+        err, user = utils.create_user(data.USER1)
+        err, group = utils.create_group(data.GROUP1[0], data.GROUP1[1], user)
+        self.assertEqual(user, group.owner)
