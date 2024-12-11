a7cff0167ab9b4f481a74a2faa8f10512de16a42
sedg <hyde.rou@yandex.ru>
2024-12-11 17:29:28 +0300

    add groupstest: griups page can visit only auth users

--- app_fixgroups/tests/test_group.py
+++ app_fixgroups/tests/test_group.py
@@ -7,9 +7,27 @@ from app_profile.tests.test_base_user import BaseUser
 
 
 class GroupTest(BaseUser):
+
+    def setUp(self):
+        err, user = utils.create_user(data.USER1)
+        self.login(data.USER1)
+        self.user = user
     
     def test_has_link_to_groups_page(self):
         response = self.client.get(reverse(data.PROFILE_PATH))
         self.assertEqual(response.status_code, 200)
         self.assertContains(
                 response, f'<a href="{reverse(data.GROUPS_PATH)}">groups</a>', html=True)
+    
+    def est_can_get_groups_page(self):
+        response = self.client.get(reverse(data.GROUPS_PATH))
+        err, user = utils.create_user(data.USER1)
+        self.login(data.USER1)
+        response = self.client.get(reverse(data.HOME_PATH))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, template.HOME_HTML)
+    
+    def test_visit_only_auth_users(self):
+        self.logout()
+        response = self.client.get(reverse(data.GROUPS_PATH))
+        self.assertRedirects(response, reverse(data.LOGIN_PATH))
--- proj_fix/template_name.py
+++ proj_fix/template_name.py
@@ -2,3 +2,5 @@ LOGIN_HTML      = 'login.html'
 HOME_HTML       = 'index.html'
 REGISTER_HTML   = 'register.html'
 PROFILE_HTML    = 'profile.html'
+
+GROUPS_HTML     = 'groups.html'
