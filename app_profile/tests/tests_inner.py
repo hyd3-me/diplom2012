5bee1a763e793933d209bcb7f7f769fceef0418a
sedg <hyde.rou@yandex.ru>
2024-12-11 12:46:58 +0300

    done innertest: can get all users from db.users

--- app_profile/tests/tests_inner.py
+++ app_profile/tests/tests_inner.py
@@ -10,3 +10,16 @@ class UserTest(TestCase):
         err, user = utils.create_user(data.USER1)
         self.assertFalse(err)
         self.assertEqual(user.username, data.USER1[0])
+    
+    def test_can_get_all_user_from_db(self):
+        # test empty db
+        err, users = utils.get_all_users()
+        self.assertFalse(err)
+        self.assertEqual(len(users), 0)
+        # create user and test equals to 1
+        err, user = utils.create_user(data.USER1)
+        err, users = utils.get_all_users()
+        self.assertEqual(len(users), 1)
+        err, user = utils.create_user(data.USER2)
+        err, users = utils.get_all_users()
+        self.assertEqual(len(users), 2)
