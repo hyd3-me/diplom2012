eadf1a1a05205c340b49355de21d0e97362157e1
sedg <hyde.rou@yandex.ru>
2024-12-11 18:19:31 +0300

    create fixgroup model

--- app_fixgroups/models.py
+++ app_fixgroups/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class FixGroup(models.Model):
+    name    = models.CharField(verbose_name='Название группы', max_length=32, unique=True)
+    h_pwd   = models.BinaryField(max_length=64)
+    token   = models.CharField(max_length=124, default='')
+    a_name  = models.CharField(max_length=16, default='')
