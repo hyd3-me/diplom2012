9be8d1eddd78efbab4b4cd3181609e6a2629f2f8
sedg <hyde.rou@yandex.ru>
2024-12-18 01:01:20 +0300

    add adddate urls and adddate_views

--- app_controldate/urls.py
+++ app_controldate/urls.py
@@ -6,4 +6,5 @@ from proj_fix import proj_data as data
 
 urlpatterns = [
     path('',                views.controldate_view, name=data.CONTROLDATE_PATH),
+    path(data.ADDDATE_PATH, views.adddate_view, name=data.ADDDATE_PATH),
     ]
--- app_controldate/views.py
+++ app_controldate/views.py
@@ -14,3 +14,27 @@ def controldate_view(request):
     if not request.method == 'GET':
         return redirect(data.ABOUT_PATH)
     return render(request, template.CONTROLDATE_HTML)
+
+def adddate_view(request):
+    if not request.user.is_authenticated:
+        return redirect(data.LOGIN_PATH)
+    if not request.method == 'GET':
+        if not request.method == 'POST':
+            return redirect(data.PROFILE_PATH)
+        group_form = GroupCreationForm(request.POST)
+        if group_form.is_valid():
+            err, group_and_staff = utils.create_group_and_staff(
+                group_form.cleaned_data.get('name'),
+                group_form.cleaned_data.get('group_pwd'),
+                request.user)
+            if not err:
+                messages.success(request, f'{data.GROUP_CREATED}')
+                return redirect(data.GROUPS_PATH)
+            else:
+                messages.error(request, f'{data.GROUP_CREATION_ERR}')
+        else:
+            messages.error(request, f'{data.INVALID_FORM}')
+    # create form for group
+    else:
+        group_form = GroupCreationForm()
+    return render(request, template.CREATE_GROUP_HTML, {'form': group_form})
