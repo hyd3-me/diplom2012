6e3611ba6ec1fbb4d15a6f9c5baf5aaf6f2a560c
sedg <hyde.rou@yandex.ru>
2024-12-18 00:51:15 +0300

    added var CONTROLDATE_HTML

--- proj_fix/template_name.py
+++ proj_fix/template_name.py
@@ -6,3 +6,5 @@ PROFILE_HTML        = 'profile.html'
 GROUPS_HTML         = 'groups.html'
 CREATE_GROUP_HTML   = 'create_group.html'
 JOIN_GROUP_HTML     = 'join_group.html'
+
+CONTROLDATE_HTML    = 'controldate.html'
