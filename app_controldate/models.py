547df99e90288c74d42d9a69159ec13315807c02
sedg <hyde.rou@yandex.ru>
2024-12-18 01:28:22 +0300

    make model for adddate

--- app_controldate/models.py
+++ app_controldate/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from app_fixgroups.models import FixGroup
 
 # Create your models here.
+
+class ControlDate(models.Model):
+
+    name    = models.CharField(verbose_name='наименование товара', max_length=64)
+    c_date  = models.DateField(auto_now_add=True)
+    e_date  = models.DateTimeField(verbose_name='окончание срока')
+    owner   = models.ForeignKey(User, on_delete=models.PROTECT)
+    group   = models.ForeignKey(FixGroup, on_delete=models.CASCADE)
