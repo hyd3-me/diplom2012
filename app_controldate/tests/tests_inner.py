f929c92f967e68cc367ca72058bebb95e5060443
sedg <hyde.rou@yandex.ru>
2024-12-18 15:11:43 +0300

    done test: has right order for end_date_list

--- app_controldate/tests/tests_inner.py
+++ app_controldate/tests/tests_inner.py
@@ -36,3 +36,21 @@ class ControlDateTest(TestCase):
         self.assertFalse(err)
         self.assertIn(records2, end_date_by_group)
         self.assertEqual(end_date_by_group.count(), 2)
+    
+    def test_has_right_order_end_date_list(self):
+        err, e_date2 = utils.now_plus_day(2)
+        err, records2 = utils.adddate(
+            data.GOOD2, e_date2, self.group_and_staff[1], self.group_and_staff[0]
+        )
+        err, e_date3 = utils.now_plus_day(3)
+        err, records3 = utils.adddate(
+            data.GOOD3, e_date3, self.group_and_staff[1], self.group_and_staff[0]
+        )
+        err, e_date1 = utils.now_plus_day(1)
+        err, records1 = utils.adddate(
+            data.GOOD1, e_date1, self.group_and_staff[1], self.group_and_staff[0]
+        )
+        err, end_date_by_group = utils.get_end_date_by_group(self.group_and_staff[0])
+        self.assertEqual(records1, end_date_by_group[0])
+        self.assertEqual(records2, end_date_by_group[1])
+        self.assertEqual(records3, end_date_by_group[2])
