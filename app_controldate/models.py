b8bc507c5035301dafbdec42095ba1ca12194b1a
sedg <hyde.rou@yandex.ru>
2024-12-18 13:11:48 +0300

    fix field User >> Staff

--- app_controldate/models.py
+++ app_controldate/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-from app_fixgroups.models import FixGroup
+from app_fixgroups.models import FixGroup, Staff
 
 # Create your models here.
 
@@ -10,5 +10,5 @@ class ControlDate(models.Model):
     name    = models.CharField(verbose_name='наименование товара', max_length=64)
     c_date  = models.DateField(auto_now_add=True)
     e_date  = models.DateTimeField(verbose_name='окончание срока')
-    owner   = models.ForeignKey(User, on_delete=models.PROTECT)
+    owner   = models.ForeignKey(Staff, on_delete=models.PROTECT)
     group   = models.ForeignKey(FixGroup, on_delete=models.CASCADE)
