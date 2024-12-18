66a1a5a61e0ad1b619bc1c715cd2f3a3693518f6
sedg <hyde.rou@yandex.ru>
2024-12-18 22:07:38 +0300

    done test: can_get_create_revision_page

--- app_revision/tests/test_revision.py
+++ app_revision/tests/test_revision.py
@@ -18,4 +18,11 @@ class TestRevision(BaseUser):
             data.GROUP1[0], data.GROUP1[1], self.user)
         response = self.client.get(reverse(data.MYGROUP_PATH, args=[group_and_staff[0].pk]))
         self.assertContains(
-                response, f'<a href="{reverse(data.CREATE_REVISION_PATH, args=[group_and_staff[0].pk])}">create revision</a>', html=True)
+            response, f'<a href="{reverse(data.CREATE_REVISION_PATH, args=[group_and_staff[0].pk])}">create revision</a>', html=True)
+    
+    def test_can_get_create_revision_page(self):
+        err, group_and_staff = utils.create_group_and_staff(
+            data.GROUP1[0], data.GROUP1[1], self.user)
+        response = self.client.get(reverse(data.CREATE_REVISION_PATH, args=[group_and_staff[0].pk]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, template.CREATEREVISION_HTML)
