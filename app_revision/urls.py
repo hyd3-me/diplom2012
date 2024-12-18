b4ea07d2ce29180aebef5cbbd93e957c8edaae24
sedg <hyde.rou@yandex.ru>
2024-12-19 00:47:47 +0300

    add url path for create_list

--- app_revision/urls.py
+++ app_revision/urls.py
@@ -7,4 +7,5 @@ from proj_fix import proj_data as data
 urlpatterns = [
     path('<int:pk>/',        views.revision_view, name=data.REVISION_PATH),
     path(f'{data.CREATE_REVISION_PATH}/<int:pk>/', views.create_revision_view, name=data.CREATE_REVISION_PATH),
+    path(f'{data.CREATE_LIST_PATH}/<int:pk>/', views.create_list_view, name=data.CREATE_LIST_PATH),
     ]
