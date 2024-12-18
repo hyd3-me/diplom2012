2452efad5366a587fd2323895a2ebc900d09a673
sedg <hyde.rou@yandex.ru>
2024-12-18 12:51:33 +0300

    add path for records date

--- app_controldate/urls.py
+++ app_controldate/urls.py
@@ -5,6 +5,7 @@ from proj_fix import proj_data as data
 
 
 urlpatterns = [
-    path('',                views.controldate_view, name=data.CONTROLDATE_PATH),
-    path(data.ADDDATE_PATH, views.adddate_view, name=data.ADDDATE_PATH),
+    path('',                    views.controldate_view, name=data.CONTROLDATE_PATH),
+    path(data.ADDDATE_PATH,     views.adddate_view, name=data.ADDDATE_PATH),
+    path(data.RECORDSDATE_PATH, views.recordsdate_view, name=data.RECORDSDATE_PATH),
     ]
