b1f27b9fdc760d98c424c8a9fc4e8c1579d3bac2
sedg <hyde.rou@yandex.ru>
2024-12-18 22:05:17 +0300

    make model for Revision

--- app_revision/models.py
+++ app_revision/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+from app_fixgroups.models import FixGroup
+
+
 # Create your models here.
+
+class Revision(models.Model):
+
+    name = models.DateField(verbose_name='дата ревизии')
+    group = models.ForeignKey(FixGroup, on_delete=models.CASCADE)
+    archive = models.BooleanField(default=False, verbose_name='архив')
+
+    class Meta:
+        ordering = ['-name']
