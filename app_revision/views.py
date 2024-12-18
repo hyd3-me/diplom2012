5205eae85609e433a2aeab24dd33622c956ec368
sedg <hyde.rou@yandex.ru>
2024-12-18 22:06:19 +0300

    make create_revision_view for processing method GET

--- app_revision/views.py
+++ app_revision/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import login, logout, authenticate
 
 from proj_fix import proj_data as data, template_name as template
 from app_profile import utils
+from .forms import CreateRevisionForm
 
 # Create your views here.
 
@@ -14,3 +15,15 @@ def revision_view(request, pk):
     if not request.method == 'GET':
         return redirect(data.ABOUT_PATH)
     return render(request, template.REVISION_HTML)
+
+def create_revision_view(request, pk):
+    if not request.user.is_authenticated:
+        return redirect(data.LOGIN_PATH)
+    if not request.method == 'GET':
+        if not request.method == 'POST':
+            return redirect(data.PROFILE_PATH)
+        pass
+    # create form for group
+    else:
+        revision_form = CreateRevisionForm()
+    return render(request, template.CREATEREVISION_HTML, {'form': revision_form})
