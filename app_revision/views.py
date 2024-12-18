2be27b9d9f23efd97bb84d15155a8b72c1702b9e
sedg <hyde.rou@yandex.ru>
2024-12-19 00:10:05 +0300

    edit create_revision_view to process POST request

--- app_revision/views.py
+++ app_revision/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
-from django.contrib.auth import login, logout, authenticate
 
 from proj_fix import proj_data as data, template_name as template
 from app_profile import utils
@@ -22,7 +21,21 @@ def create_revision_view(request, pk):
     if not request.method == 'GET':
         if not request.method == 'POST':
             return redirect(data.PROFILE_PATH)
-        pass
+        revision_form = CreateRevisionForm(request.POST)
+        if revision_form.is_valid():
+            err, group = utils.get_group_by_id(pk)
+            if not err:
+                err, revision = utils.create_revision(
+                    revision_form.cleaned_data.get('name'), group)
+                if not err:
+                    messages.success(request, f'{data.CREATE_REVISION_SUCCESS}')
+                    return redirect(reverse(data.MYGROUP_PATH, args=[pk]))
+                else:
+                    messages.error(request, f'{data.CREATE_REVISION_FAILED}')
+            else:
+                messages.error(request, f'{data.GROUP_NOT_FOUND}')
+        else:
+            messages.error(request, f'{data.INVALID_FORM}')
     # create form for group
     else:
         revision_form = CreateRevisionForm()
