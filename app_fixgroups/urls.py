78aa8e5a47d11e67a7f6aff3512873afbaeec79e
sedg <hyde.rou@yandex.ru>
2024-12-18 19:20:08 +0300

    add urls for mugroup_path

--- app_fixgroups/urls.py
+++ app_fixgroups/urls.py
@@ -7,5 +7,6 @@ from proj_fix import proj_data as data
 urlpatterns = [
     path('',                views.groups_view, name=data.GROUPS_PATH),
     path(data.CREATE_GROUP_PATH, views.create_group_view, name=data.CREATE_GROUP_PATH),
-    path(data.JOIN_GROUP_PATH,   views.join_group_view, name=data.JOIN_GROUP_PATH)
+    path(data.JOIN_GROUP_PATH,   views.join_group_view, name=data.JOIN_GROUP_PATH),
+    path(f'{data.MYGROUP_PATH}/<int:pk>/',  views.mygroup_view, name=data.MYGROUP_PATH),
     ]
