235a2743b31a274edc9d7fd8910af548c6589ba0
sedg <hyde.rou@yandex.ru>
2024-12-19 00:49:05 +0300

    done test: has_link_to_create_list

--- app_revision/tests/test_revision.py
+++ app_revision/tests/test_revision.py
@@ -51,3 +51,11 @@ class TestRevision(BaseUser):
         response = self.client.get(reverse(data.REVISION_PATH, args=[revision.pk]))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, template.REVISION_HTML)
+    
+    def test_has_link_to_create_list(self):
+        err, group_and_staff = utils.create_group_and_staff(
+            data.GROUP1[0], data.GROUP1[1], self.user)
+        err, today = utils.get_today()
+        err, revision = utils.create_revision(today, group_and_staff[0])
+        response = self.client.get(reverse(data.REVISION_PATH, args=[revision.pk]))
+        self.assertContains(response,  f'<a href="{reverse(data.CREATE_LIST_PATH, args=[revision.pk])}">create list</a>', html=True)
