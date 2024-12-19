e6e8a01243bb9e532518023c216cef20db5bb735
sedg <hyde.rou@yandex.ru>
2024-12-19 03:32:51 +0300

    done test: has_link_to_create_record

--- app_revision/tests/test_revision.py
+++ app_revision/tests/test_revision.py
@@ -99,3 +99,12 @@ class TestRevision(BaseUser):
         response = self.client.get(reverse(data.LIST_PATH, args=[list_.pk]))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, template.LIST_HTML)
+    
+    def test_has_link_to_create_record(self):
+        err, group_and_staff = utils.create_group_and_staff(
+            data.GROUP1[0], data.GROUP1[1], self.user)
+        err, today = utils.get_today()
+        err, revision = utils.create_revision(today, group_and_staff[0])
+        err, list_ = utils.create_list(data.LIST1, revision)
+        response = self.client.get(reverse(data.LIST_PATH, args=[list_.pk]))
+        self.assertContains(response,  f'<a href="{reverse(data.CREATE_RECORD_PATH, args=[list_.pk])}">create record</a>', html=True)
