28c4fad9359ee339fa363858209bc4ddd9d81c81
sedg <hyde.rou@yandex.ru>
2024-12-19 03:55:23 +0300

    make Record model

--- app_revision/models.py
+++ app_revision/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from app_fixgroups.models import FixGroup
+from app_fixgroups.models import FixGroup, Staff
 
 
 # Create your models here.
@@ -18,3 +18,12 @@ class List(models.Model):
 
     name = models.CharField(verbose_name='имя списка', max_length=32)
     revision = models.ForeignKey(Revision, on_delete=models.CASCADE)
+
+class Record(models.Model):
+
+    name = models.CharField(verbose_name='имя записи', max_length=64)
+    barcode = models.CharField(verbose_name='баркод', max_length=32)
+    count = models.PositiveSmallIntegerField(verbose_name='количество')
+    owner = models.ForeignKey(Staff, on_delete=models.PROTECT)
+    _list = models.ForeignKey(List, on_delete=models.CASCADE)
+    note = models.TextField(verbose_name='добавить примечание', max_length=124, blank=True)
