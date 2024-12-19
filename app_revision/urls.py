2c10ffddfa0f4e0b6ff04648ecd11bcaec303a0c
sedg <hyde.rou@yandex.ru>
2024-12-20 00:37:47 +0300

    add url path for search_record

--- app_revision/urls.py
+++ app_revision/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path(f'{data.CREATE_LIST_PATH}/<int:pk>/', views.create_list_view, name=data.CREATE_LIST_PATH),
     path(f'{data.LIST_PATH}/<int:pk>/', views.list_view, name=data.LIST_PATH),
     path(f'{data.CREATE_RECORD_PATH}/<int:pk>/', views.create_record_view, name=data.CREATE_RECORD_PATH),
+    path(data.SEARCH_RECORD_PATH,   views.search_record_view, name=data.SEARCH_RECORD_PATH),
     ]
