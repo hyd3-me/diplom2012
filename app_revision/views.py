339428433e00528579fb290fe2fb17c65079affa
sedg <hyde.rou@yandex.ru>
2024-12-19 03:01:49 +0300

    make create_list_view

--- app_revision/views.py
+++ app_revision/views.py
@@ -3,7 +3,7 @@ from django.contrib import messages
 
 from proj_fix import proj_data as data, template_name as template
 from app_profile import utils
-from .forms import CreateRevisionForm
+from .forms import CreateRevisionForm, CreateListForm
 
 # Create your views here.
 
@@ -45,5 +45,20 @@ def create_list_view(request, pk):
     if not request.user.is_authenticated:
         return redirect(data.LOGIN_PATH)
     if not request.method == 'GET':
-        return redirect(data.ABOUT_PATH)
+        if not request.method == 'POST':
+            return redirect(data.PROFILE_PATH)
+        list_form = CreateListForm(request.POST)
+        if list_form.is_valid():
+            err, revision = utils.get_revision_by_id(pk)
+            if not err:
+                err, list_ = utils.create_list(list_form.cleaned_data.get('name'), revision)
+                if not err:
+                    messages.success(request, f'{data.LIST_CREATED_SUCCESS}')
+                    return redirect(reverse(data.REVISION_PATH, args=[pk]))
+                else:
+                    messages.error(request, f'{data.CREATE_LIST_FAILED}')
+            else:
+                messages.error(request, f'{data.REVISION_NOT_FOUND}')
+        else:
+            messages.error(request, f'{data.INVALID_FORM}')
     return render(request, template.CREATE_LIST_HTML, {'revision_id': pk})
