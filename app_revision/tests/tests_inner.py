feecf7a720432b85f9f1e92a7039a3dfe611f2eb
sedg <hyde.rou@yandex.ru>
2024-12-19 11:55:04 +0300

    done test: get_list_by_id

--- app_revision/tests/tests_inner.py
+++ app_revision/tests/tests_inner.py
@@ -32,3 +32,11 @@ class RevisionTest(TestCase):
         err, list_ = utils.create_list(data.LIST1, revision)
         err, record = utils.create_record(data.RECORD1, data.BARCODE1, data.GOOD_COUNT1, data.TEST_NOTE1, list_, self.group_and_staff[1])
         self.assertFalse(err)
+    
+    def test_can_get_list_by_id(self):
+        err, today = utils.get_today()
+        err, revision = utils.create_revision(today, self.group_and_staff[0])
+        err, _list = utils.create_list(data.LIST1, revision)
+        err, _list_from_db = utils.get_list_by_id(_list.pk)
+        self.assertFalse(err)
+        self.assertEqual(_list, _list_from_db)
