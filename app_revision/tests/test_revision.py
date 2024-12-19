295176a307c26dc78f8d78e1e58fcda1d319c1ae
sedg <hyde.rou@yandex.ru>
2024-12-19 03:21:30 +0300

    done test: can_get_list_page

--- app_revision/tests/test_revision.py
+++ app_revision/tests/test_revision.py
@@ -89,3 +89,13 @@ class TestRevision(BaseUser):
         response = self.client.post(url, {'name': data.LIST1, }, follow=True)
         self.assertRedirects(response, reverse(data.REVISION_PATH, args=[revision.pk]))
         self.assertContains(response, data.LIST_CREATED_SUCCESS)
+    
+    def test_can_get_list_page(self):
+        err, group_and_staff = utils.create_group_and_staff(
+            data.GROUP1[0], data.GROUP1[1], self.user)
+        err, today = utils.get_today()
+        err, revision = utils.create_revision(today, group_and_staff[0])
+        err, list_ = utils.create_list(data.LIST1, revision)
+        response = self.client.get(reverse(data.LIST_PATH, args=[list_.pk]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, template.LIST_HTML)
