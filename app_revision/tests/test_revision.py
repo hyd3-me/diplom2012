17f0d2621730268c37bf3e84b774fffc66fddec8
sedg <hyde.rou@yandex.ru>
2024-12-19 03:10:54 +0300

    done test: create_list_page contain CreateListForm

--- app_revision/tests/test_revision.py
+++ app_revision/tests/test_revision.py
@@ -69,6 +69,17 @@ class TestRevision(BaseUser):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, template.CREATE_LIST_HTML)
     
+    def test_create_list_HTML_contain_form(self):
+        err, group_and_staff = utils.create_group_and_staff(
+            data.GROUP1[0], data.GROUP1[1], self.user)
+        err, today = utils.get_today()
+        err, revision = utils.create_revision(today, group_and_staff[0])
+        response = self.client.get(reverse(data.CREATE_LIST_PATH, args=[revision.pk]))
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.context['form'], forms.CreateListForm)
+        form = forms.CreateListForm()
+        self.assertContains(response, form.as_p(), html=True)
+    
     def test_can_create_list_via_http(self):
         err, group_and_staff = utils.create_group_and_staff(
             data.GROUP1[0], data.GROUP1[1], self.user)
